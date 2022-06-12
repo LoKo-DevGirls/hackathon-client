2cb0ed306fca1ecf188ceb2e9f79616f008f011c
hannah-developer <hannahchoi.developer@gmail.com>
2022-06-12 10:14:19 +0100

    additems request + redirect

--- hackathon-client/src/components/AddItem/AddItem.jsx
+++ hackathon-client/src/components/AddItem/AddItem.jsx
@@ -1,9 +1,9 @@
 import React, {useState} from 'react'
 import styles from './AddItem.module.scss'
-
+import Select from 'react-select'
+import { Navigate } from 'react-router-dom';
 
 export const AddItem = () => {
-
     const [formData, setFormData] = useState({
         itemName: "",
         isRecyclable: true,
@@ -14,59 +14,80 @@ export const AddItem = () => {
 
     const [recycle, setRecycle] = useState(true);
     const toggleRecycle = () => setRecycle(!recycle);
+    const [redirect, setRedirect] = useState(false);
 
     const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
+        e.target? setFormData({ ...formData, [e.target.name]: e.target.value }) : setFormData({ ...formData, [e.name]: e.value });
+    };
+
+    const addItemFetch = async (data) => {
+        try {
+            const response = await fetch("https://on-the-ground-api.herokuapp.com/item", {
+                body: JSON.stringify({
+                    itemName: data.itemName,
+                    itemImage: 'foo',
+                    isRecyclable: data.isRecyclable,
+                    howToRecycle: data.steps,
+                    upcycleVideo: 'foo',
+                    councilId: data.council,
+                    categoryId: data.category,
+                }),
+                headers: { "Content-Type": "application/json" },
+                method: "POST"
+            });
+            const finalData = await response.json()
+            console.log(finalData)
+            alert('Thanks for the submission. Your data is now waiting for the approval')
+            setRedirect(true);
+        } catch (err) {
+            console.error(err);
+        }
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        addFetch(formData);//TODO: Not yet implemented
+        addItemFetch(formData);
     };
+
+    const categoryOptions = [
+        { value: '1', name: 'category', label: 'Paper' },
+        { value: '2', name: 'category', label: 'Plastic' },
+        { value: '3', name: 'category', label: 'Metal' },
+        { value: '4', name: 'category', label: 'Glass' },
+        { value: '5', name: 'category', label: 'Electronic Devices' }
+    ]
+
+    const councilOptions = [
+        {value: "555", name: 'council', label: 'Hackney'},
+        {value: "666", name: 'council', label: 'Southwark'},
+    ]
 
     return (
         <div className={styles.addItem}>
             <header>
-                <h3>Add Item</h3>
+                <h1>Add Item</h1>
             </header>
             <form onSubmit={handleSubmit}>
-                <section>
+                <label htmlFor='itemName'>Type Item name</label> 
                 <input type='text' name='itemName' pattern="^[A-Za-z0-9]{3,10}$" onChange={(e)=> handleChange(e)}/>
-                </section>
-                <section>
-                <span>Is it recyclable?</span>
-                <input type="radio" id='recyclable' value={true} name="isRecyclable" onClick={toggleRecycle} onChange={(e)=> handleChange(e)}/> 
-                <label htmlFor='recyclable'>Yes</label>          
-                <input type="radio" id='notRecyclable' value={false} name="isRecyclable" onClick={toggleRecycle} onChange={(e)=> handleChange(e)}/> 
-                <label htmlFor='noRecyclable'>No</label>  
-                </section>        
+                <label htmlFor='isRecyclable'>Is it recyclable?</label> 
+                <input type="radio" checked={recycle} id='recyclable' value={true} name="isRecyclable" onClick={toggleRecycle} onChange={(e)=> handleChange(e)}/> 
+                <label htmlFor='isRecyclable'>Yes</label>          
+                <input type="radio" checked={!recycle} id='notRecyclable' value={false} name="isRecyclable" onClick={toggleRecycle} onChange={(e)=> handleChange(e)}/> 
+                <label htmlFor='isRecyclable'>No</label>  
                 {recycle && (
-                    <section>
-                    <select name="category" onChange={(e)=> handleChange(e)}>
-                        <option disabled selected value></option>
-                        <option value="paper">Paper</option>
-                        <option value="plastic">Plastic</option>
-                        <option value="metal">Metal</option>
-                        <option value="glass">Glass</option>
-                        <option value="electronic">Electronic Device</option>
-                    </select>
-                    <label htmlFor='steps'>How to recycle</label>   
-                    <input type='text' id='steps' onChange={(e)=> handleChange(e)}/>
-                    </section>
-                    )
-                }
-                <section>
-                <label htmlFor='council'>Select your council</label>  
-                <select name="council" onChange={(e)=> handleChange(e)}>
-                        <option disabled selected value></option>
-                        <option value="hackney">Hackney</option>
-                        <option value="islington">Islington</option>
-                        <option value="camden">Camden</option>
-                        <option value="southwark">Southwark</option>
-                </select>
-                </section>
-                <input type='submit' value='Submit'/>
+                    <>
+                    <label htmlFor='category'>Choose the category</label> 
+                        <Select name='category' options={categoryOptions} onChange={(e)=> handleChange(e)} />
+                        <label htmlFor='steps'>How to recycle</label>   
+                        <input type='text' name='steps' id='steps' onChange={(e)=> handleChange(e)}/>
+                        </>
+                )}        
+                <label htmlFor='council'>Choose your council</label>  
+                <Select name='council' options={councilOptions} onChange={(e)=> handleChange(e)}/>
+                <input className={styles.submitInput} type='submit' value='Submit'/>
             </form>
+            {redirect && <Navigate replace to="/" />}
         </div>
     );
 };
