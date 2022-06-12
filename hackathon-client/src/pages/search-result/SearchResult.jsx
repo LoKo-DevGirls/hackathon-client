3a518d5fcba4a7d636778e8d0b464db0877508bf
Jade.Kim <jade.kim@travelflan.com>
2022-06-12 07:55:58 +0100

    fetch item info

--- hackathon-client/src/pages/search-result/SearchResult.jsx
+++ hackathon-client/src/pages/search-result/SearchResult.jsx
@@ -1,51 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './SearchResult.module.scss'
 import { HowToRecycle } from '../../components/SearchResult/HowToRecycleCard'
 import { ItemSummaryInfo } from '../../components/SearchResult/ItemSummaryInfo'
 import { MoreItemsCard } from '../../components/SearchResult/MoreItemsCard'
 import { UpcycleVideoCard } from '../../components/SearchResult/UpcycleVideoCard'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
 
-const dummyItem = {
-  itemId: "bbb",
-  itemName: "Water Bottle",
-  itemImage: "https://londonrecycles.co.uk/wp-content/uploads/2020/08/Plastic-bottle.jpg",
-  isRecyclable: true,
-  howToRecycle:
-    "1. Empty bottle of residual contents.\n2. Keep the label and cap on.\n3. Put it into a recycling bin",
-  upcycleVideo: "",
-  councilId: 1,
-  categoryId: 1,
-  status: "approved"
-}
+const apiUrl = "https://on-the-ground-api.herokuapp.com"
 
 export const SearchResult = () => {
-  const {
-    itemId,
-    itemName,
-    itemImage,
-    isRecyclable,
-    howToRecycle,
-    upcycleVideo,
-    councilId,
-    categoryId
-  } = dummyItem
+  const { itemName, councilId } = useParams()
+  const [searchResult, setSearchResult] = useState(null)
 
   const councilName = 'Southwark'
   const categoryName = 'Plastic'
   const moreItemList = [{itemId:1}, {itemId:2}, {itemId:3}, {itemId:4}]
 
+  useEffect(() => {
+
+    const getSearchedItem = async () => {
+      try {
+        const { data } = await axios.get(`${apiUrl}/item`, {
+          method: 'GET',
+          params: {
+            search: itemName,
+            councilId,
+          },
+          headers: {
+            "Content-type": "application/json; charset=UTF-8"
+          }
+        })
+
+        const result = data[0]
+        setSearchResult(result)
+
+      } catch (err) {
+        console.error(err)
+      }
+    }
+
+    getSearchedItem()
+
+    return () => {
+    }
+  }, [])
+  
+  if (!searchResult) return null;
+
   return (
     <div className={styles.searchResultPage}>
       <ItemSummaryInfo
-        itemName={itemName}
-        isRecyclable={isRecyclable}
-        councilName={councilName}
-        categoryName={categoryName}
-        imagePath={itemImage}
+        itemName={searchResult.itemName}
+        isRecyclable={searchResult.isRecyclable}
+        councilName={searchResult.councilName}
+        categoryName={searchResult.categoryName}
+        imagePath={searchResult.itemImage}
       />
       <div className={styles.cardsWrapper}>
-        {howToRecycle &&<HowToRecycle howToRecycleInfo={howToRecycle} />}
-        {upcycleVideo && <UpcycleVideoCard videoLink={upcycleVideo} />}
+        {searchResult.howToRecycle &&<HowToRecycle howToRecycleInfo={searchResult.howToRecycle} />}
+        {searchResult.upcycleVideo && <UpcycleVideoCard videoLink={searchResult.upcycleVideo} />}
         <MoreItemsCard itemList={moreItemList} />
       </div>
     </div>
